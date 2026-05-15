7523663188b25360a08872a3f6430d4d45ba6219
mochiimofflez <152946161+mochiimofflez@users.noreply.github.com>
2026-05-15 16:35:20 -0700

    Final final fix for syntax errors in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-\import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { LandingPage } from './components/LandingPage';
 import { Home } from './pages/Home';
 import { WikiView } from './pages/WikiView';
@@ -19,4 +19,4 @@ export function App() {
       <Route path='/Admin' element={<AdminPage />} />
     </Routes>
   );
-}\
+}
